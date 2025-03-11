52622c170345e50b6f77f6727f3e2bfc62340074
nwb725 <nwb725@alumni.ku.dk>
2025-03-11 19:09:45 +0100

    Initial commit

---
+++ blackjack/target/debug/deps/blackjack-0aa5839428f4ee7f.d
@@ -0,0 +1,9 @@
+/home/malte/Desktop/BlackJackCLG/blackjack/target/debug/deps/libblackjack-0aa5839428f4ee7f.rmeta: src/main.rs Cargo.toml
+
+/home/malte/Desktop/BlackJackCLG/blackjack/target/debug/deps/blackjack-0aa5839428f4ee7f.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
