813c8c4e5f3e7d5d400eda0a866de86154056ff6
nwb725 <nwb725@alumni.ku.dk>
2025-03-16 11:47:15 +0100

    Add nix flake file

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A terminal-based Blackjack game in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";  # Change to `aarch64-linux` for ARM (e.g., Apple M1/M2)
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "blackjack";
+      version = "1.0.0";
+      src = ./.;
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+      };
+    };
+  };
+}
